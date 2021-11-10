7706e0632052fe0518bb898b36b953ab9f736b41
dmitry <supranovich2@gmail.com>
2021-11-10 18:19:27 +0300

    add jenkinsfile

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,6 +23,3 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/${NAME}-${VERSION}.jar
-sudo -i
-scp ${NAME}-${VERSION}.jar ubuntu@10.0.11.11:/home/ubuntu
