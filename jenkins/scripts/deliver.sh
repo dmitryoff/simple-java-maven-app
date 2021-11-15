5420cfaab3deb6532467021f20c48d614dd9d952
dmitryoff <87693922+dmitryoff@users.noreply.github.com>
2021-11-15 09:15:13 +0300

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -24,4 +24,4 @@ echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 java -jar target/${NAME}-${VERSION}.jar
-
+scp ${NAME}-${VERSION}.jar ubuntu@54.216.16.56:/home/ubuntu
