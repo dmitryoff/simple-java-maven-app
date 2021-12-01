827c6f483a4582c3d514b98a489d962909b89a43
dmitryoff <87693922+dmitryoff@users.noreply.github.com>
2021-12-01 20:50:05 +0300

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo 'The following Maven command installs your Maven-built Java applicationn'
+echo 'The following Maven command installs your Maven-built Java application'
 echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
@@ -19,5 +19,10 @@ echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 set +x
+
+echo 'The following command runs and outputs the execution of your Java'
+echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
+set -x
 java -jar target/${NAME}-${VERSION}.jar
+
 
